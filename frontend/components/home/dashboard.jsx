1957661d85b08b3df72e83134f51332672eec038
Jonathan <yoniyaniv@gmail.com>
2020-06-03 10:03:29 -0400

    footer there, but lackluster

--- frontend/components/home/dashboard.jsx
+++ frontend/components/home/dashboard.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import MenuBar from '../menu_bar/menu_bar_container'
 import HeaderNotifications from '../header_notifications/header_notifications_container'
 import BannerCta from './splash/banner_cta'
+import FooterMenu from '../menu_bar/footer_menu'
 
 export default () => (
     <>
         <HeaderNotifications />
         <MenuBar />
         <BannerCta />
+        <FooterMenu />
     </>
 )
---
+++ frontend/components/menu_bar/footer_menu.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+export default () => (
+    <div className="footer">
+        <div className="footer-header">
+            <p>Reach me on social media: </p>
+            <span className="social-links">
+                <a href="#">LinkedIn</a>
+                <a href="#">AngelsList</a>
+                <a href="#">GitHub</a>
+            </span>
+        </div>
+        <div className="cols">
+            <div className="left-col">
+                <ul className="footer-menu">
+                    <li><p>Contact</p></li>
+                    <li><a href="jgyaniv.github.io">Portfolio Website</a></li>
+                    <li><a href="mailto:jgyaniv@gmail.com">jgyaniv@gmail.com</a></li>
+                    <li><a href="#">(551)265-5763</a></li>
+                </ul>
+            </div>
+            <div className="mid-col">
+                <ul className="footer-menu">
+                    <li><p>A Task Tabbit Clone</p></li>
+                    <li><a href="taskrabbit.com">taskrabbit.com</a></li>
+                </ul>
+            </div>
+            <div className="right-col">
+
+            </div>
+        </div>
+    </div>
+)
